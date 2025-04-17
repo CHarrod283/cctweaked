4868dad175888c5f9054a6012470ca4a892143a2
charrod <carter.harrod283@gmail.com>
2025-04-17 18:27:31 -0500

    add some extra info and handle enum

--- main/base/scan_items.lua
+++ main/base/scan_items.lua
@@ -30,25 +30,58 @@ end
 
 function SerializeInventory(inventory)
     expect(1, inventory, "table")
-    local data = "{\"peripheral_name\": \"" .. inventory.peripheral_name .. "\",\"computer_id\": " .. inventory.computer_id .. " ,\"inventory_type\":\"".. inventory.inventory_type .. "\", \"inventory\":["
-    
+    local data = "{"
+    local data = data .. "\"peripheral_name\": \"" .. inventory.peripheral_name .. "\","
+    local data = data .. "\"computer_id\":" .. inventory.computer_id .. ","
+    if inventory.inventory_type == "storage" then
+        local data = data .. "\"inventory_type\":\"".. inventory.inventory_type .. "\","
+    elseif inventory.inventory_type == "input" then
+        local data = data .. "\"inventory_type\":{\"".. inventory.inventory_type .. "\": {\"destination\":" .. inventory.destination .."\"}},"
+    elseif inventory.inventory_type == "output" then
+        local data = data .. "\"inventory_type\":{\"".. inventory.inventory_type .. "\": {\"source\":" .. inventory.source .."\"}},"
+    end
+    local data = data .. "\"inventory_type\":\"".. inventory.inventory_type .. "\","
+
+    local data = data .. "\"inventory\":["
     for k,v in pairs(inventory.list()) do
-        data = data.. "{\"slot\": " .. k ..", \"name\":\"" .. v.name .. "\", \"count\": ".. v.count .. "},"
+        data = data.. "{\"slot\":" .. k ..",\"name\":\"" .. v.name .. "\", \"count\":".. v.count .. "},"
     end
     data = string.sub(data, 1, #data - 1)
     data = data .. "]}"
     return data
 end
 
-function GetStoragePeripheral(name, type)
+function GetStoragePeripheral(name)
     expect(1, name, "string")
-    expect(2, type, "string")
 
     local peripheral = peripheral.wrap(name)
     peripheral.peripheral_name = name
     peripheral.computer_id = os.getComputerID()
-    peripheral.inventory_type = type
+    peripheral.inventory_type = "storage"
     return peripheral
 end
 
-Main(GetStoragePeripheral("functionalstorage:controller_extension_0", "input"))
+function GetStorageInputPeripheral(name, destination)
+    expect(1, name, "string")
+
+    local peripheral = peripheral.wrap(name)
+    peripheral.peripheral_name = name
+    peripheral.computer_id = os.getComputerID()
+    peripheral.inventory_type = "input"
+    peripheral.destination = destination
+    return peripheral
+end
+
+
+function GetStorageOutputPeripheral(name, source)
+    expect(1, name, "string")
+
+    local peripheral = peripheral.wrap(name)
+    peripheral.peripheral_name = name
+    peripheral.computer_id = os.getComputerID()
+    peripheral.inventory_type = "output"
+    peripheral.source = source
+    return peripheral
+end
+
+Main(GetStoragePeripheral("functionalstorage:controller_extension_0"))
--- rustserver/src/main.rs
+++ rustserver/src/main.rs
@@ -1,7 +1,40 @@
 use std::fmt::Debug;
+use axum::http::StatusCode;
 use axum::Router;
 use axum::routing::{get, post};
 use tracing::{error, info};
+
+
+
+#[derive(Debug, serde::Deserialize, serde::Serialize)]
+struct InventoryReport {
+    common_name: String,
+    computer_id: i64,
+    inventory: Vec<InventoryItem>,
+    peripheral_name: String,
+    inventory_type: InventoryType,
+}
+
+#[derive(Debug, serde::Deserialize, serde::Serialize)]
+struct InventoryItem {
+    slot: i64,
+    name: String,
+    count: i64,
+}
+
+#[derive(Debug, serde::Deserialize, serde::Serialize)]
+enum InventoryType {
+    #[serde(rename = "input")]
+    Input{
+        destination: String,
+    },
+    #[serde(rename = "output")]
+    Output{
+        source: String,
+    },
+    #[serde(rename = "storage")]
+    Storage
+}
 
 #[tokio::main]
 async fn main() {
@@ -9,7 +42,7 @@ async fn main() {
     let app = Router::new()
         .route("/", get(|| async {"hello world"}))
         .route("/", post(print_json));
-    
+
     let listener =  tokio::net::TcpListener::bind("127.0.0.1:3000").await.unwrap_or_else(|e| {
         error!("Failed to bind to address: {}", e);
         std::process::exit(1);
@@ -20,7 +53,66 @@ async fn main() {
     });
 }
 
-async fn print_json(json: axum::Json<serde_json::Value>) -> String {
-    info!("Received JSON: {:?}", json);
-    format!("Received JSON: {:?}", json)
+async fn print_json(axum::Json(json): axum::Json<serde_json::Value>) -> (StatusCode, Option<String>) {
+    match serde_json::from_value::<InventoryReport>(json) {
+        Ok(report) => {
+            info!("deserialized report: {:?}", report);
+            (StatusCode::OK, None)
+        }
+        Err(e) => {
+            error!("Failed to serialize JSON: {}", e);
+            (StatusCode::BAD_REQUEST, Some(format!("Failed to serialize JSON: {}", e)))
+        }
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_serialize() {
+        let report = InventoryReport {
+            common_name: "Test Computer".to_string(),
+            computer_id: 12345,
+            inventory: vec![
+                InventoryItem {
+                    slot: 1,
+                    name: "Test Item".to_string(),
+                    count: 10,
+                },
+            ],
+            peripheral_name: "Test Peripheral".to_string(),
+            inventory_type: InventoryType::Input {
+                destination: "Test Destination".to_string(),
+            },
+        };
+
+        let serialized = serde_json::to_string(&report).unwrap();
+        assert_eq!(
+            serialized,
+            r#"{"common_name":"Test Computer","computer_id":12345,"inventory":[{"slot":1,"name":"Test Item","count":10}],"peripheral_name":"Test Peripheral","inventory_type":{"input":{"destination":"Test Destination"}}}"#
+        );
+        
+        let report = InventoryReport {
+            common_name: "Test Computer".to_string(),
+            computer_id: 12345,
+            inventory: vec![
+                InventoryItem {
+                    slot: 1,
+                    name: "Test Item".to_string(),
+                    count: 10,
+                },
+            ],
+            peripheral_name: "Test Peripheral".to_string(),
+            inventory_type: InventoryType::Storage
+        };
+        
+        let serialized = serde_json::to_string(&report).unwrap();
+        assert_eq!(
+            serialized,
+            r#"{"common_name":"Test Computer","computer_id":12345,"inventory":[{"slot":1,"name":"Test Item","count":10}],"peripheral_name":"Test Peripheral","inventory_type":"storage"}"#
+        );
+    }
 }
